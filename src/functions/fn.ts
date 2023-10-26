9aca659f1bba1a7e5e10cc6f0e4a182207b57952
0xWaleed <imwaleed@outlook.sa>
2023-10-26 19:29:08 +0300

    refactor: rename js files according to ts conventions :recycle:

--- src/functions/fn.ts
+++ src/functions/fn.ts
@@ -9,36 +9,24 @@ export async function getRandomFile() {
 	return files[index];
 }
 
-export const file_num = (num: number, power10: number) => {
-    const power = 10 ** power10;
-    const getNumberArr = (n: number) => String(n).split("");
-    let _0day: number[] = [];
-
-    getNumberArr(power).map((_) => _0day.push(0));
-    _0day = _0day.slice(0, -getNumberArr(num).length);
-    _0day.push(num);
-
-    return _0day.join("");
-};
-
-	// factory to get the code challenge structure
+// factory to get the code challenge structure
 export const challengeFactory = async () => {
 	const file = await getRandomFile();
 	const content = await readFile(`${DATA_DIR}/${file}`);
 	const contentAsString = content.toString();
 
-	const file_arr = contentAsString.split("@@@");
-	const file_obj = {
-		question: file_arr[0].split("\n")[0],
-		choices: file_arr[1].split("\n").filter((e) => !["", " "].includes(e)),
+	const splitedContent = contentAsString.split("@@@");
+	const challengeObject = {
+		question: splitedContent[0].split("\n")[0],
+		choices: splitedContent[1].split("\n").filter((e: string) => !["", " "].includes(e)),
 			answer: Number(
-				file_arr[2]
+				splitedContent[2]
 				.split("\n")
-				.filter((e) => !["", " "].includes(e))
+				.filter((e: string) => !["", " "].includes(e))
 				.join()
 		),
 	};
 
-	return file_obj;
+	return challengeObject;
 };
 
