74e1796f3e6c062e788c54d2a621cbe8ca15eb60
0xWaleed <imwaleed@outlook.sa>
2023-11-05 20:32:23 +0300

    feat(repo): ensure to get next message unless all consumed :sparkles:

--- src/repo/discord-repo.ts
+++ src/repo/discord-repo.ts
@@ -1,6 +1,34 @@
-import {Client, TextChannel} from "discord.js";
+import {Client, Collection, Message, TextChannel} from "discord.js";
 import * as config from "../config.ts";
 import {Challenge} from "../types/challenge.ts";
+
+const consumedMessages: Record<string, boolean> = {};
+
+function pickNextMessage(collection: Collection<string, Message<true>>): Message {
+	const size = collection.size;
+
+	const consumedKeys = Object.keys(consumedMessages);
+	if (consumedKeys.length >= size) {
+		consumedKeys.forEach(key => {
+			delete consumedMessages[key];
+		});
+	}
+
+	let out;
+
+	do {
+		out = collection.random();
+
+		if (!out) {
+			throw new Error("No challenge found");
+		}
+
+	} while (consumedMessages[out.id]);
+
+	consumedMessages[out.id] = true;
+
+	return out;
+}
 
 export async function getRandomChallenge(client: Client): Promise<Challenge> {
 	const channel = await client.channels.fetch(config.CHANNEL_SOURCE_ID) as TextChannel;
@@ -11,11 +39,7 @@ export async function getRandomChallenge(client: Client): Promise<Challenge> {
 
 	const messages = await channel.messages.fetch({limit: 100});
 
-	const message = messages.random();
-
-	if (!message) {
-		throw new Error(`No challenge found in channel [${config.CHANNEL_SOURCE_ID}]`);
-	}
+	const message = pickNextMessage(messages);
 
 	const content = message.content.replace(/(^```|```$)/g, "");
 
